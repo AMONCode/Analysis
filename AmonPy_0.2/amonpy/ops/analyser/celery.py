663b5a01e17e2c0cbad10df5482558dfd029b15b
gtesic <gordana@tesic.net>
2014-04-18 15:16:41 -0400

    set json as a serializer for message exchange

--- AmonPy_0.2/amonpy/ops/analyser/celery.py
+++ AmonPy_0.2/amonpy/ops/analyser/celery.py
@@ -10,9 +10,9 @@ app = Celery('analyser',
 # Optional configuration, see the application user guide.
 app.conf.update(
     CELERY_TASK_RESULT_EXPIRES=3600,
-  #  CELERY_TASK_SERIALIZER='json',
-   # CELERY_ACCEPT_CONTENT=['json'],  # Ignore other content
-    #CELERY_RESULT_SERIALIZER='json',
+    CELERY_TASK_SERIALIZER='json',
+    CELERY_ACCEPT_CONTENT=['json'],  # Ignore other content
+    CELERY_RESULT_SERIALIZER='json',
 )
 
 if __name__ == '__main__':
