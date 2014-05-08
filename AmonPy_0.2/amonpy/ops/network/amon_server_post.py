2417e91063580c85d2e3866cc9e249d5a4d2c469
gtesic <gordana@tesic.net>
2014-05-08 17:01:07 -0400

    reconstructed DB writing functions to be a thread safe

--- AmonPy_0.2/amonpy/ops/network/amon_server_post.py
+++ AmonPy_0.2/amonpy/ops/network/amon_server_post.py
@@ -8,7 +8,7 @@ performs DB transactions in a separate threat, thus keeping the code asynchronou
 """
 from twisted.internet import reactor, defer
 from twisted.web.resource import Resource
-from twisted.web.server import Site
+from twisted.web.server import Site, NOT_DONE_YET
 from twisted.enterprise import adbapi
 
 import cgi, os, getopt, sys
@@ -31,120 +31,104 @@ import dbase.voevent_to_event
 
 from analyser.runanal import *
 
-#global counter
-#counter = 0
-class WriteEvent(object):
-    counter = 0
-    def __init__(self):
-        self.HostFancyName='localhost'
-        self.UserFancyName='yourname'
-        self.PasswordFancy='yourpass'
-        self.DBFancyName='AMON_test2'
-        self.eventlist = []
-        self.paramlist = []
-        self.microsec = 0.
-        self.dbpool = adbapi.ConnectionPool("MySQLdb", db = self.DBFancyName, 
-                                            user = self.UserFancyName, 
-                                            passwd = self.PasswordFancy, 
-                                            host = self.HostFancyName)
-        WriteEvent.counter +=1
-        print "Counter %s" % (WriteEvent.counter,) 
-        # initialize task for AMON analysis in analyser.runanal
-        #self.ana=AnalRT() 
-        
-    # run dbpool interaction instead of run query, which allows for a series of queries 
-    # to be run one after anther  
-    
-    def _writeEventParam(self,transaction, event, evparam):
-        # this will run in a separate thread, allowing us to use series of queries 
-        # without blocking the rest of the code
-        
-        # need to add event to self.eventlist because of a later callback 
-        #( e.g. printResult bellow) 
-        
-        if (len(self.eventlist)) > 0:
-            self.eventlist.pop()  
-        self.eventlist.append(event[0])
-        
-        # take microsecond part from datetime because of the older versions of mysql
-        
-        if '.' in str(self.eventlist[0].datetime):
-            self.microsec=int(float('.'+str(self.eventlist[0].datetime).split('.')[1])*1000000)
-                #print 'microseconds %d' % microsec
-        else:
-            self.microsec=0.
-          
-        # add parameter to self.parameterlist because of later callbacks
-            
-        if (len(self.paramlist)) > 0:
-            self.paramlist.pop()  
-        self.paramlist.append(evparam[0])    
-        
-        transaction.execute("""INSERT INTO event VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,
-                            %s,%s,%s,%s,%s,%s,%s,%s,%s)""",(self.eventlist[0].stream,
-                            self.eventlist[0].id,
-                            self.eventlist[0].rev,
-                            self.eventlist[0].datetime, 
-                            self.microsec, 
-                            self.eventlist[0].dec,
-                            self.eventlist[0].RA, 
-                            self.eventlist[0].sigmaR, 
-                            self.eventlist[0].nevents,
-                            self.eventlist[0].deltaT,
-                            self.eventlist[0].sigmaT,
-                            self.eventlist[0].false_pos,
-                            self.eventlist[0].pvalue,
-                            self.eventlist[0].type,
-                            self.eventlist[0].point_RA,
-                            self.eventlist[0].point_dec,
-                            self.eventlist[0].longitude, 
-                            self.eventlist[0].latitude,
-                            self.eventlist[0].elevation,
-                            self.eventlist[0].psf_type,
-                            0)) 
-        transaction.execute("""INSERT INTO parameter VALUES (%s,%s,%s,%s,%s,%s)""",
-                           (self.paramlist[0].name,
-                            self.paramlist[0].value,
-                            self.paramlist[0].units,
-                            self.paramlist[0].event_eventStreamConfig_stream, 
-                            self.paramlist[0].event_id,self.paramlist[0].event_rev))
-    
-    def writeEventParam(self, event, evparam):
-        return self.dbpool.runInteraction(self._writeEventParam, event, evparam)                                             
-    
-    def printResult(self,result):
-        print "This event is written"
-        print self.eventlist[0].stream, self.eventlist[0].id, self.eventlist[0].rev
-        print "This parameter is written"
-        print self.paramlist[0].event_eventStreamConfig_stream, self.paramlist[0].event_id, \
-            self.paramlist[0].event_rev
-        
-        #after Event and Parameter are written in DB, it is safe to send 
-        # a message to the analyser about new event to be read from DB and added to 
-        # analyses 
-            
-        AnalRT().apply_async((self.eventlist[0].stream, self.eventlist[0].id, self.eventlist[0].rev),
-                                link_error=error_handler.s())      
-         
-    def printError(self,error):
-        print "Got Error: %r" % error
-        error.printTraceback()                              
-    
-    def Finish(self):
-        # do not call this function in reaal-time settings 
-        self.dbpool.close()  
-                                                      
-
 class EventPage(Resource):
     isLeaf = True
-        
-    w = WriteEvent()
+    #from amon_server_post4 import WriteEvent 
+    #w = WriteEvent()
     
     # initiate celery task that will send message to analysis server 
     # about new incoming event
     #n=AnalRT()
+    
+    HostFancyName='localhost'
+    UserFancyName='yourname'
+    PasswordFancy='yourpass'
+    DBFancyName='AMON_test2'
+    eventlist = []
+    paramlist = []
+    microsec = 0.
+    counter = 1
+    
+    print "Event page is %d" % counter
+    dbpool = adbapi.ConnectionPool("MySQLdb", db = DBFancyName, 
+                                            user = UserFancyName, 
+                                            passwd = PasswordFancy, 
+                                            host = HostFancyName,
+                                            cp_min=1,
+                                            cp_max=1)
+        
+        # initialize task for AMON analysis in analyser.runanal
+    ana=AnalRT()
 
     def render_POST(self, request):
+    
+        def _writeEventParam(transaction, event, evparam):
+            # this will run in a separate thread, allowing us to use series of queries 
+            # without blocking the rest of the code
+                
+            # take microsecond part from datetime because of the older versions of mysql
+        
+            if '.' in str(event[0].datetime):
+                microsec=int(float('.'+str(event[0].datetime).split('.')[1])*1000000)
+                    #print 'microseconds %d' % microsec
+            else:
+                microsec=0.0
+                      
+            transaction.execute("""INSERT INTO event VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,
+                            %s,%s,%s,%s,%s,%s,%s,%s,%s)""",(event[0].stream,
+                            event[0].id,
+                            event[0].rev,
+                            event[0].datetime, 
+                            microsec, 
+                            event[0].dec,
+                            event[0].RA, 
+                            event[0].sigmaR, 
+                            event[0].nevents,
+                            event[0].deltaT,
+                            event[0].sigmaT,
+                            event[0].false_pos,
+                            event[0].pvalue,
+                            event[0].type,
+                            event[0].point_RA,
+                            event[0].point_dec,
+                            event[0].longitude, 
+                            event[0].latitude,
+                            event[0].elevation,
+                            event[0].psf_type,
+                            0)) 
+                            
+            transaction.execute("""INSERT INTO parameter VALUES (%s,%s,%s,%s,%s,%s)""",
+                           (evparam[0].name,
+                            evparam[0].value,
+                            evparam[0].units,
+                            evparam[0].event_eventStreamConfig_stream, 
+                            evparam[0].event_id,evparam[0].event_rev))
+            return event                
+                           
+        def writeEventParam(event, evparam):
+            return EventPage.dbpool.runInteraction(_writeEventParam, event, evparam) 
+            #dd.addCallbacks(self.printResult, self.printError)
+            #return dd 
+        
+        def printResult(result):
+            print "This event is written"
+            print result[0].stream, result[0].id, result[0].rev
+            
+            #after Event and Parameter are written in DB, it is safe to send 
+            # a message to the analyser about new event to be read from DB and added to 
+            # analyses 
+            
+            EventPage.ana.apply_async((result[0].stream, result[0].id, result[0].rev),
+                                link_error=error_handler.s())      
+            
+        def printError(error):
+            print "Got Error: %r" % error
+            error.printTraceback()                              
+    
+        def Finish():
+            # do not call this function in reaal-time settings 
+            EventPage.dbpool.close()
+        
         self.headers = request.getAllHeaders()
         print self.headers
         try:
@@ -171,17 +155,22 @@ class EventPage(Resource):
         evParam[0].forprint()
         
         os.remove(path+"server_tmp_events/"+fname)
-        # write to DB
-        t1 = time()                                                                
-        #w = WriteEvent(event)
+
+        #t1 = time()                                                                
                   
         #write event to DB (does it in a separate thread)
-        d = EventPage.w.writeEventParam(event, evParam)
+        d = writeEventParam(event, evParam)
+        
         # send a meesage to the analyser in case event and parameter
         # are written to DB, otherwise print error
-        d.addCallbacks(EventPage.w.printResult, EventPage.w.printError)
-        t2 = time() 
-        print '   DB writing time: %.5f seconds' % float(t2-t1) 
         
-        return request.content.read()
+        d.addCallbacks(printResult, printError)
+        
+        #t2 = time() 
+        #print '   DB writing time: %.5f seconds' % float(t2-t1) 
+        
+        request.finish()
+        return NOT_DONE_YET
+        #return request.content.read()
 
+                               
