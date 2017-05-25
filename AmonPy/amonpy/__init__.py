acae3901cee61d7c200aa099c302f273d847365f
hayalaso <hayalaso@mtu.edu>
2017-05-25 16:26:40 -0400

    Adding some imports in init to test the installation

---
+++ AmonPy/amonpy/__init__.py
@@ -0,0 +1,7 @@
+from amonpy.anal.cluster import Fisher, Fisher_tp, Fisher_doublet
+from amonpy.anal.analysis import *
+
+from amonpy.dbase.db_classes import Alert, EventStreamConfig, AlertConfig2, AlertLine,Parameter
+from amonpy.dbase.db_metadata import DBMetadata
+
+from amonpy.ops.analyser.runanal import AnalRT
