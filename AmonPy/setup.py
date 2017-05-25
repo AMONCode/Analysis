c76e37a4ff6d7faa58ce474921d070325d1db414
hayalaso <hayalaso@mtu.edu>
2017-05-25 15:59:38 -0400

    some changes in the setup.py to add functions from ops

--- AmonPy/setup.py
+++ AmonPy/setup.py
@@ -1,12 +1,21 @@
 # setup.py
 
 from distutils.core import setup
-    
-setup(name = "AmonPy", 
-      version = "1.4", 
+
+setup(name = "AmonPy",
+      version = "1.4",
       description='AMON Analysis Code',
       author='Miles Smith, Gordana Tesic',
       author_email='mus44@psu.edu, gut10@psu.edu',
-      packages = ['amonpy', 'amonpy.anal', 'amonpy.dbase',  'amonpy.sim', 'amonpy.tools'],
+      packages = ['amonpy',
+                  'amonpy/anal',
+                  'amonpy/dbase',
+                  'amonpy/sim',
+                  'amonpy/tools',
+                  'amonpy/ops',
+                  'amonpy/ops/analyser',
+                  'amonpy/ops/analyser/network',
+                  'amonpy/ops/analyser/network/scripts'
+                 ],
       scripts = ['amonpy/ops/run_archival.py', 'amonpy/ops/run_basic_sim.py'],
       )
