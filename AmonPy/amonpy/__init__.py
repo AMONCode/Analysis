492087f68647c79538b6a542c00d0156df42db56
hayalaso <hayalaso@mtu.edu>
2017-05-26 18:11:44 -0400

    fixing some problems with setup.py

--- AmonPy/amonpy/__init__.py
+++ AmonPy/amonpy/__init__.py
@@ -3,5 +3,6 @@ from amonpy.anal.analysis import *
 
 from amonpy.dbase.db_classes import Alert, EventStreamConfig, AlertConfig2, AlertLine,Parameter
 from amonpy.dbase.db_metadata import DBMetadata
+from amonpy.dbase import db_read, db_write, db_delete, db_classes, db_metadata
 
 from amonpy.ops.analyser.runanal import AnalRT
