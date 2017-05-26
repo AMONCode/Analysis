4910b4ed50188bda9b9827f6acba452a3b13bf05
hayalaso <hayalaso@mtu.edu>
2017-05-26 18:14:07 -0400

    fixing some problems with setup.py

--- AmonPy/setup.py
+++ AmonPy/setup.py
@@ -15,7 +15,6 @@ setup(name = "AmonPy",
                   'amonpy/ops',
                   'amonpy/ops/analyser',
                   'amonpy/ops/network/',
-                  'amonpy/ops/network/scripts'
                  ],
       scripts = ['amonpy/ops/run_archival.py', 'amonpy/ops/run_basic_sim.py'],
       )
