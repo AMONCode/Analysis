d509627d6b939ed21bc857b251d4c718c31c2b9f
Hugo Ayala <hgchavo05@gmail.com>
2018-05-01 13:49:52 -0400

    adding changes from fork to official code

--- AmonPy/amonpy/dev/testevents/create_voevents.py
+++ AmonPy/amonpy/dev/testevents/create_voevents.py
@@ -4,12 +4,6 @@ use this VOEvents to test AMON server and real-time analysis
 """
 from __future__ import absolute_import
 import sys
-sys.path.append("../../")
-sys.path.append("../")
-sys.path.append("../../..")
-sys.path.append("../../dbase")
-sys.path.append('../../tools')
-sys.path.append('../../analysis')
 
 from datetime import datetime, timedelta
 
@@ -18,8 +12,8 @@ from amonpy.dbase.db_classes import Event, Parameter
 import amonpy.dbase.event_to_voevent as event_to_voevent
 
 HostFancyName='localhost'
-UserFancyName='yourhost'
-PasswordFancy='yourpass'
+UserFancyName='root'
+PasswordFancy='hawcfis05h'
 DBFancyName='AMON_test2'
 
 event_streams = [0,1,3,7]
@@ -34,8 +28,10 @@ TimeStop  = str(validStop)
 
 events=db_read.read_event_timeslice_streams(event_streams, TimeStart,TimeSlice,HostFancyName,
                                     UserFancyName,PasswordFancy,DBFancyName)
-#parameters = []                                    
+#parameters = []
 nevents=len(events)
+
+##Make fname a variable
 
 for kk in xrange(nevents):
     parameters=[Parameter("energy",events[kk].stream, events[kk].id, events[kk].rev)]
@@ -44,19 +40,17 @@ for kk in xrange(nevents):
     #print xml1
     fname = ''
     if (events[kk].stream == 0):
-        fname='/yourpath/events_icecube/event_%s.xml' % (kk,)
-    elif (events[kk].stream == 1): 
-        fname='/yourpath/events_antares/event_%s.xml' % (kk,) 
-    elif (events[kk].stream == 3): 
-        fname='/yourpath/events_auger/event_%s.xml' % (kk,) 
-    elif (events[kk].stream == 7): 
-        fname='/yourpath/events_hawc/event_%s.xml' % (kk,)
+        fname='/Users/hugo/AMON/Test_DB/server_tmp_events/events_icecube/event_%s.xml' % (kk,)
+    elif (events[kk].stream == 1):
+        fname='/Users/hugo/AMON/Test_DB/server_tmp_events/events_antares/event_%s.xml' % (kk,)
+    elif (events[kk].stream == 3):
+        fname='/Users/hugo/AMON/Test_DB/server_tmp_events/events_auger/event_%s.xml' % (kk,)
+    elif (events[kk].stream == 7):
+        fname='/Users/hugo/AMON/Test_DB/server_tmp_events/events_hawc/event_%s.xml' % (kk,)
     else:
         print "unsupported stream for now"
-        pass            
+        pass
     print fname
     f1=open(fname, 'w+')
     f1.write(xml1)
     f1.close()
-    
-                                        
