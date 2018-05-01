d12f30b5e5c01f0790c4d64cccba38076a29e10c
Hugo Ayala <hgchavo05@gmail.com>
2018-05-01 15:14:26 -0400

    changing email_alerts.py. Includes changes from both fork and official

--- AmonPy/amonpy/dbase/email_alerts.py
+++ AmonPy/amonpy/dbase/email_alerts.py
@@ -1,8 +1,8 @@
-import sys
-
-# sys.path.append('../db_classes')
+import sys, os
 
 from amonpy.dbase.db_classes import *
+from amonpy.tools.config import AMON_CONFIG
+from amonpy.analyses.amon_streams import streams, alert_streams
 
 import smtplib
 from email.mime.text import MIMEText
@@ -10,18 +10,18 @@ import netrc, ConfigParser
 #import yowsup_run as yowsup_run
 
 def alert_email(alert, params):
-    config_fname = '../amon.ini'
-    Config = ConfigParser.ConfigParser()
-    Config.read(config_fname)
-    ehe_hese_emails = eval(Config.get('mailing_list', 'ehe_hese'))
-    sub_emails = eval(Config.get('mailing_list', 'sub_ehe_hese'))
-    sub_cut_emails = eval(Config.get('mailing_list', 'sub_cut_ehe_hese'))
-    nrc_fname = Config.get('dirs', 'amonpydir') + '.netrc'
-    prodMachine = eval(Config.get('machine', 'prod'))
+    #config_fname = '../amon.ini'
+    #Config = ConfigParser.ConfigParser()
+    #Config.read(config_fname)
+    ehe_hese_emails = AMON_CONFIG.get('mailing_list','ehe_hese') #eval(Config.get('mailing_list', 'ehe_hese'))
+    sub_emails = AMON_CONFIG.get('mailing_list','sub_ehe_hese')
+    sub_cut_emails = AMON_CONFIG.get('mailing_list','sub_cut_ehe_hese')
+    nrc_fname = os.path.join(AMON_CONFIG.get('dirs','amonpydir'), '.netrc')
+    prodMachine = AMON_CONFIG.get('machine','prod')
     nrc = netrc.netrc(nrc_fname)
     stream = alert[0].stream
 
-    if (stream==10):
+    if (stream==streams['IC-HESE']):
         for i in range(len(params)):
             if (params[i].name== 'event_id'):
                 event_id=int(params[i].value)
@@ -43,10 +43,10 @@ def alert_email(alert, params):
         dec = alert[0].dec
         content = 'HESE_charge = '+str(charge)+'\n'+'HESE_signal_trackness = '+str(signal_trackness)+'\n'+'HESE_ra = '+str(ra)+'\n'+'HESE_dec = '+str(dec)+'\n'+'HESE_event_time = '+str(dateutc)+'\n'+'HESE_run_id = '+str(run_id)+'\n'+'HESE_event_id = '+str(event_id)+'\n'+'HESE_angular_error_50 = '+str(src_error_50)+'\n'+'HESE_angular_error_90 = '+str(src_error_90)+'\n'
 
-	if prodMachine==True:
-           title_msg = 'HESE event'
-        else: 
-           title_msg = 'Test from Dev machine: HESE'
+        if prodMachine==True:
+            title_msg = 'HESE event'
+        else:
+            title_msg = 'Test from Dev machine: HESE'
         if ((charge>=6000.0) and (signal_trackness>=0.1)):
             FROM = nrc.hosts['hese_ehe_gmail'][0] + '@gmail.com'
             PASS = nrc.hosts['hese_ehe_gmail'][2]
@@ -59,7 +59,7 @@ def alert_email(alert, params):
             FROM = nrc.hosts['gmail'][0] + '@gmail.com'
             PASS = nrc.hosts['gmail'][2]
             TO = sub_emails
-    if (stream==11):
+    if (stream==streams['IC-EHE']):
         for i in range(len(params)):
             if (params[i].name== 'event_id'):
                 event_id=int(params[i].value)
@@ -77,10 +77,10 @@ def alert_email(alert, params):
         dec = alert[0].dec
         content = 'EHE_signalness = '+str(signalness)+'\n'+'EHE_ra = '+str(ra)+'\n'+'EHE_dec = '+str(dec)+'\n'+'EHE_event_time = '+str(dateutc)+'\n'+'EHE_run_id = '+str(run_id)+'\n'+'EHE_event_id = '+str(event_id)+'\n'+'EHE_angular_error_50 = '+str(src_error_50)+'\n'
 
-	if prodMachine==True:
-           title_msg = 'EHE event'
-        else: 
-           title_msg = 'Test from Dev machine: EHE'
+        if prodMachine==True:
+            title_msg = 'EHE event'
+        else:
+            title_msg = 'Test from Dev machine: EHE'
         if alert[0].type=="observation":
             FROM = nrc.hosts['hese_ehe_gmail'][0] + '@gmail.com'
             PASS = nrc.hosts['hese_ehe_gmail'][2]
@@ -115,13 +115,7 @@ def alert_email(alert, params):
     #yowsup_run.send_message("18147699477",content)
 
 def alert_email_content(alert,content,title_msg):
-    #nrc_fname = os.path.join(AMON_CONFIG.get('dirs','amonpydir'), '.netrc')
-    #nrc = netrc.netrc(nrc_fname)
-    config_fname = '../amon.ini'
-    Config = ConfigParser.ConfigParser()
-    Config.read(config_fname)
-    nrc_fname = Config.get('dirs', 'amonpydir') + '.netrc'
-    prodMachine = eval(Config.get('machine', 'prod'))
+    nrc_fname = os.path.join(AMON_CONFIG.get('dirs','amonpydir'), '.netrc')
     nrc = netrc.netrc(nrc_fname)
 
     if alert[0].type == "observation":
