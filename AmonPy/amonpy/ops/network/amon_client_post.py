686add76430e9cb7bea2d5124d3f7131f7bcb4be
Hugo Ayala <hgchavo05@gmail.com>
2018-05-02 14:10:02 -0400

    change amon_client_post.py

--- AmonPy/amonpy/ops/network/amon_client_post.py
+++ AmonPy/amonpy/ops/network/amon_client_post.py
@@ -1,5 +1,5 @@
 """@package amon_client_post_events
-client that sends events to the server using HTTP 
+client that sends events to the server using HTTP
 protocol with method=POST
 """
 import sys, getopt, os, shutil, logging, datetime
@@ -13,6 +13,8 @@ from twisted.internet.defer import Deferred, succeed
 from twisted.internet.protocol import Protocol
 from twisted.web.client import Agent, FileBodyProducer
 from twisted.web import http_headers
+
+from amonpy.tools.config import AMON_CONFIG
 
 from zope.interface import implements
 
@@ -39,20 +41,20 @@ def printError(failure):
     d=Deferred()
     d.addCallbacks(printNotSent)
     print >>sys.stderr, failure
-    
+
 def stop(result):
     reactor.stop()
-    
+
 def moveFile(path,fname):
-    shutil.move(path+fname, path+"archive/"+fname) 
-    print "File %s sent" % (fname,)  
-    
+    shutil.move(path+fname, path+"archive/"+fname)
+    print "File %s sent" % (fname,)
+
 def printSent(fname):
-    print "File %s sent" % (fname,) 
-     
+    print "File %s sent" % (fname,)
+
 def printNotSent(filename):
-    print "File %s not sent" % (fname,)  
-    
+    print "File %s not sent" % (fname,)
+
 def check_open_fds():
     fds = []
     soft, hard = resource.getrlimit(resource.RLIMIT_NOFILE)
@@ -67,11 +69,11 @@ def check_open_fds():
 def check_for_files(hostport, eventpath):
     # check a directory with events (eventpath) for an oldest xml file
     # if found post it to the server (hostport) using HTTP POST protocol
-    
+
     # check for number of open file descriptors first, OS X is limited by default to 256
-    # each OS has its own limitation, file descriptors are open not only when the file 
+    # each OS has its own limitation, file descriptors are open not only when the file
     # is open, but for each new socket/connection opened as well
-    
+
     fds = []
     soft, hard = resource.getrlimit(resource.RLIMIT_NOFILE)
     for fd in range(0, soft):
@@ -80,16 +82,16 @@ def check_for_files(hostport, eventpath):
         except IOError:
             continue
         fds.append(fd)
-        
+
     if (len(fds)<=soft):
         host=hostport
         path=eventpath
-    
+
         agent = Agent(reactor)
-    
+
         files = sorted(os.listdir(path), key=lambda p: os.path.getctime(os.path.join(path, p)))
         files_xml=[]
-      
+
         for filename in files:
             if (os.path.isdir(path+filename) or filename[0]=='.' or filename.find(".log") !=-1):
                 pass
@@ -97,35 +99,35 @@ def check_for_files(hostport, eventpath):
                 files_xml.append(filename)
             else:
                 pass
-            
+
         if len(files_xml)>0:
-            oldest = files_xml[0] 
-            
+            oldest = files_xml[0]
             try:
                 datafile=open(path+oldest)
                 #data=datafile.read()
                 #lenght_data=str(len(data))
-            
+
                 #body = StringProducer(data)
-                
                 body=FileBodyProducer(datafile)
                 length_data=str(body.length)
                 headers = http_headers.Headers({'User-Agent': ['Twisted HTTP Client'],
-                                            'Content-Type':['text/xml'], 
+                                            'Content-Type':['text/xml'],
                                             'Content-Length': [length_data],
                                             'Content-Name':[oldest]})
                 d = agent.request('POST', host, headers, bodyProducer=body)
                 # on success it returns Deferred with a response object
                 d.addCallbacks(printResource, printError)
-                #shutil.move(path+oldest, path+"archive/"+oldest)
+                #shutil.move(path+oldest,path+"archive/"+oldest)
                 #datafile.close()
                 #print "Event %s sent" % (oldest,)
             except:
-                log.msg("Error parsing file %s " % (oldest,))
-            
+                log.msg("Error parsing file %s " % (path+oldest,))
+            shutil.move(path+oldest, os.path.join(AMON_CONFIG.get('dirs','serverdir'),"server_archive_events",oldest))
+            #shutil.move(path+oldest, "/Users/hugo/AMON/Test_new_server/server_archive_events/"+oldest)
+            #shutil.move(path+oldest, "/storage/home/hza53/work/AMON/Test_server/server_archive_events/"+oldest)
+            #datafile.close()
+
         else:
             pass
     else:
-        pass          
-    
-
+        pass
