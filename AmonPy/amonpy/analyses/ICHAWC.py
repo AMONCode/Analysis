9e9b411d38f4e75bf73295335921d7fbd86c980b
Hugo Ayala <hgchavo05@gmail.com>
2018-05-31 22:12:21 -0400

    debugging

--- AmonPy/amonpy/analyses/ICHAWC.py
+++ AmonPy/amonpy/analyses/ICHAWC.py
@@ -339,7 +339,7 @@ def coincAnalysisHWC(new_event):
             #sinTh = np.sqrt(1-cosTh**2)
             #bkgIC = -1*sinTh*probBkgIC(cosTh)
             pvalIC = pHEN(np.sin(np.deg2rad(dec2)))
-            datalist.append([streams['IC-Singlet'],dec2,ra2,poserr2,psfIC,pd.to_datetime(e.datetime), bkgIC , pvalIC,fprd)])
+            datalist.append([streams['IC-Singlet'],dec2,ra2,poserr2,psfIC,pd.to_datetime(e.datetime), bkgIC , pvalIC,fprd])
 
     alldatalist.append(datalist)
 
