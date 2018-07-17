89853d92a3c8a6a8a2ade3d98b31e563717f0684
Hugo Ayala <hgchavo05@gmail.com>
2018-07-17 14:19:51 -0400

    making a tool for angular separation

--- AmonPy/amonpy/tools/angularsep.py
+++ AmonPy/amonpy/tools/angularsep.py
@@ -1,9 +1,12 @@
 import numpy as np
 
 #Angular separation of two angles in the sphere
-def spcang(ra1,ra2,dec1,dec2):
+def spcang(ra1,ra2,dec1,dec2,degree=True):
     dec1 = np.deg2rad(dec1)
     dec2 = np.deg2rad(dec2)
     DeltaRA = np.deg2rad(ra1-ra2)
     sep = np.arccos(np.cos(dec1)*np.cos(dec2)*np.cos(DeltaRA) + np.sin(dec1)*np.sin(dec2))
-    return np.rad2deg(sep)
+    if degree:
+        return np.rad2deg(sep)
+    else:
+        return sep
