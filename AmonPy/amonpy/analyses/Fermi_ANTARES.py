8fb2871f2e28e765aa5b229bc430c62e70a5abf2
cft114 <cft114@psu.edu>
2018-09-12 14:04:06 -0400

    fixed bug in the portion that sends emails

--- AmonPy/amonpy/analyses/Fermi_ANTARES.py
+++ AmonPy/amonpy/analyses/Fermi_ANTARES.py
@@ -674,7 +674,7 @@ while n<len(xmlpathholder):
     #if  we do not trigger an error, send all new events to GCN
     print 'sending email %d' % (n,)
     subject='New Fermi-ANTARES Alert'
-    body='New Fermi-ANTARES alert detected with a lambda of %f. XML sent to GCN is attached \n  Enjoy' % (lamlist(n))
+    body='New Fermi-ANTARES alert detected with a lambda of %f. XML sent to GCN is attached \n  Enjoy' % (lamlist[n])
     send_email_attach(subject,body,['cft114@psu.edu'],xmlpathholder[n])
     
     cmd=['comet-sendvo']
