990b1ddb89d3e0c5e834536248bbd28c22ce3083
Hugo Ayala <hgchavo05@gmail.com>
2019-02-25 12:19:48 -0500

    change in ivorn so that it is easier to parse in GCN

--- AmonPy/amonpy/analyses/ICHAWC.py
+++ AmonPy/amonpy/analyses/ICHAWC.py
@@ -472,7 +472,7 @@ def ic_hawc(new_event=None):
             f1=open(filen, 'w+')
 
             #Create Alert xml file
-            VOAlert = Alert2VOEvent([new_alert],'gamma_nu_coinc','Gamma-Nu multimessenger coincidence')
+            VOAlert = Alert2VOEvent([new_alert],'gamma_nu_coinc',new_alert.id,new_alert.id,'Gamma-Nu multimessenger coincidence')
             someparams = VOAlert.MakeDefaultParams([new_alert])
             VOAlert.WhatVOEvent(someparams)
             VOAlert.MakeWhereWhen([new_alert])
