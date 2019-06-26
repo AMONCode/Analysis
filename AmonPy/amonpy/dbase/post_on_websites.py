8e2e484cc12d4494f08b436560109df2b4e9c752
Grégoire <tgregoir@apc.in2p3.fr>
2019-06-26 12:47:09 -0400

    Add HAWCGRB to openamon and correct some unit mistakes

--- AmonPy/amonpy/dbase/post_on_websites.py
+++ AmonPy/amonpy/dbase/post_on_websites.py
@@ -14,7 +14,7 @@ def post_on_OpenAMON(post, particle):
     me = "amon.psu@gmail.com"
     pwd = "amonpassword"
     object_post = "%s alert!"  % (particle)
-    object_notif_trig = "%s_alert" % (particle)# ! Do not change that without changing the Zapier settings
+    object_notif_trig = "%s alert!" % (particle)# ! Do not change that without changing the Zapier settings
     
     send_email(site_email,me,pwd,object_post,post) # Sends the post
     send_email(notif_email,me,pwd,object_notif_trig,"") # Sends a notification (arrives within 5 min)
@@ -65,7 +65,7 @@ def ICgoldbronze_to_OpenAMON(event, params):
 
 This event has a %.1f%% chance to be a neutrino of astrophysical origin. The background noise fluctuation should produce a similar event every %d days in average.
 
-Its energy is estimated to be around %d TeV [1. %d times the average energy released in nuclear fission of one Uranium-235 atom] and its source has a 90%% probability to be located within a zone of %.2f square deg [2. %d times smaller than the moon angular coverage].
+Its energy is estimated to be around %d TeV [1. %d times the average energy released in nuclear fission of one Uranium-235 atom] and its source is located with a 90%% probability to be within a zone of %.2f square deg [2. %d times smaller than the moon angular coverage].
 
 Is it the result of a blazar, a supernovae...? This alert has been sent to astronomers around the world so they can point their telescopes toward the source of this event. An other signal from a different messenger could allow to understand the mechanisms involved.
 
@@ -73,13 +73,46 @@ Is it the result of a blazar, a supernovae...? This alert has been sent to astro
 This event has been detected on %s UTC (Coordinated Universal Time).
 The algorithm estimates its source location to be around the following coordinates: right ascension = %.4f degrees, declination = %.4f degrees with a 90%% probability to be within a disk of radius %.1f arcmin and a 50%% probability to be within a disk of %.1f arcmin radius.
 The uncertainty in the location of the event is based on statistical uncertainty only, not accounting for the systematic error which should be smaller.
-The estimated energy is %.5e TeV.
+The estimated energy is %s TeV.
 The background noise should produce an event at least as significant %.2f times per year which leads to a probability of %.2f%% that this event is a track-like neutrino of astrophysical origin.
 [category alert]""" % (signalness*100., (1./far)*365.25, energy/1000., energy/0.2, src_error_90_solid_angle*(180./pi)**2,
-src_error_90_solid_angle/moon_solid_angle, str(event.datetime), float(event.RA), float(event.dec), degrees(src_error_90)*60.,
-degrees(src_error_50)*60., E_sci_notat, far, signalness*100.)
+src_error_90_solid_angle/moon_solid_angle, str(event.datetime), float(event.RA), float(event.dec), src_error_90*60.,
+src_error_50*60., E_sci_notat, far, signalness*100.)
+# TODO adapt moon thing for larger error
 
     post_on_OpenAMON(post, "Neutrino")
+
+
+def HAWCGRB_to_OpenAMON(event):
+
+    run_id = str(event.id)[:-8]
+    event_id = str(event.id)[-4:]
+    tag=str(event.datetime)[2:4]+str(event.datetime)[5:7]+str(event.datetime)[8:10]
+
+    # TODO add an image?
+    # TODO add the stellarium thing # <a href="https://stellarium-web.org/skysource/Vega?fov=120.00&amp;date=2019-06-15T01:30:13Z&amp;lat=39.86&amp;lng=-74.17&amp;elev=0">in the direction of Vega</a>[1. This is a broad direction, see the complete set of information for a precise one]!"
+    # TODO change "degree" into real degree sign (make it python 2 and 3 compatible)
+
+    ############ Post template ############################
+    moon_solid_angle=6.67e-5 # steradians
+    src_error_90_solid_angle=(1.-cos(radians(event.sigmaR)))*2.*pi # steradians
+    
+    post="""## An intriguing event has just been detected by the HAWC gamma ray telescope!
+
+The background noise fluctuation should produce a similar event every %d days in average. Its source is located with a 90%% probability to be within a zone of %.2f square deg [2. %d times smaller than the moon angular coverage].
+
+Is it the result of a blazar, a supernovae...? This alert has been sent to astronomers around the world so they can point their telescopes toward the source of this event. An other signal from a different messenger could allow to understand the mechanisms involved.
+
+### Complete set of information:
+This event has been detected on %s UTC (Coordinated Universal Time).
+The algorithm estimates its source location to be around the following coordinates: right ascension = %.4f degrees, declination = %.4f degrees with a 90%% probability to be within a disk of radius %.1f arcmin.
+The uncertainty in the location of the event is based on statistical uncertainty only, not accounting for the systematic error which should be smaller.
+The background noise should produce an event at least as significant %.2f times per year.
+[category alert]""" % ((1./event.false_pos)*365.25, src_error_90_solid_angle*(180./pi)**2,
+src_error_90_solid_angle/moon_solid_angle, str(event.datetime), float(event.RA), float(event.dec), event.sigmaR*60.,
+event.false_pos)
+
+    post_on_OpenAMON(post, "Gamma ray burst")
 
 
 def ICgoldbronze_to_AMONalerts(event, params):
@@ -121,7 +154,9 @@ EVENT_NUM: %s
 SRC_RA: %0.4f [deg]
 SRC_DEC: %0.4f [deg]
 SRC_ERROR_90: %0.2f [arcmin radius, stat-only, 90%% containment]
+
 SRC_ERROR_50: %0.2f [arcmin radius, stat-only, 50%% containment]
+
 REVISION: %s
 ENERGY:  %.4e [TeV]
 SIGNALNESS: %.4f [dn]
@@ -130,7 +165,7 @@ COMMENTS: %s event.
 COMMENTS: The position error is statistical only, there is no systematic added.
 [category %s]
 [tags IceCube-%s]""" % (notice_type, event_datetime, str(event.stream), str(run_id),
-    str(event_id), event.RA, event.dec, src_error_90, src_error_50, str(event.rev), energy/1000.,
+    str(event_id), event.RA, event.dec, src_error_90*60., src_error_50*60., str(event.rev), energy/1000.,
     signalness, far, notice_type, notice_type, tag)
 
     post_title = "%s alert %s" % (notice_type, tag)
@@ -163,7 +198,7 @@ delta_T: %s [sec]
 COMMENTS: HAWC burst monitor event.
 [category HAWC Burst Monitor]
 [tags HAWC-%s]""" % ("HAWC Burst Monitor", str(event.datetime), str(event.stream), str(run_id), str(event_id),
-    event.RA, event.dec, event.sigmaR, str(event.rev), event.false_pos, event.pvalue, str(event.deltaT), tag) 
+    event.RA, event.dec, event.sigmaR*60., str(event.rev), event.false_pos, event.pvalue, str(event.deltaT), tag) 
      
     post_title = "HAWC Burst Monitor alert %s" % (tag)
 
