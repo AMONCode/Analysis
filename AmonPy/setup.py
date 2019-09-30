ffba6192346f7a9dc70d0f98ff9337cb85ad3eae
Hugo Ayala <hgchavo05@gmail.com>
2019-09-30 14:01:34 -0400

    using 2to3 to start making changes to make code compatible with Py3

--- AmonPy/setup.py
+++ AmonPy/setup.py
@@ -10,7 +10,7 @@ https://github.com/pypa/sampleproject
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 from os import path
-from ConfigParser import ConfigParser
+from configParser import ConfigParser
 import uuid
 import socket
 import subprocess
@@ -50,18 +50,18 @@ class AMONInstall(install):
 
             cp.set("alerts","slack_token","a_token")
 
-            if raw_input("Do you wish to setup the amon server configuration? (Y/N): ").upper() == "Y":
+            if input("Do you wish to setup the amon server configuration? (Y/N): ").upper() == "Y":
                   for section in cp.sections():
                         for k,v in cp.items(section):
-                              answer = raw_input("%s : %s = [%s]" % (section, k, v)).strip()
+                              answer = input("%s : %s = [%s]" % (section, k, v)).strip()
                               if answer != "":
                                     cp.set(section, k, answer)
                   with open("amonpy/amon.ini", "w") as f:
                         cp.write(f)
 
 
-            if raw_input("Do you wish to setup the database? (Y/N): ").upper() == "Y":
-                  print "root login for the mysql server..."
+            if input("Do you wish to setup the database? (Y/N): ").upper() == "Y":
+                  print("root login for the mysql server...")
                   u,h,p = cp.get("database", "username"), cp.get("database", "host_name"), cp.get("database", "password")
                   d1, d2 = cp.get("database", "archive_dbname"), cp.get("database", "realtime_dbname")
                   # Create the dbs
