47231c17adcc96e298ff80641af76a39bb354125
Hugo Ayala <hza53@psu.edu>
2020-04-06 17:26:37 +0000

    tests trying to improve them

--- AmonPy/amonpy/dbase/test/test_db_read.py
+++ AmonPy/amonpy/dbase/test/test_db_read.py
@@ -3,6 +3,7 @@ import sys
 
 from amonpy.dbase import db_read
 from amonpy.tools import *
+from amonpy.tools.config import AMON_CONFIG
 import unittest
 """@package test_db_read
 Unit tests for db_read module.
@@ -14,25 +15,25 @@ class TestDBRead(unittest.TestCase):
     def setUp(self):
 # insert your host, user name and password here!
         self.realArchive=0
-        self.HostFancyName='yourhost'
-        self.UserFancyName='yourname'
-        self.PasswordFancy='yourpass'
-        self.DBFancyName='AMON_test1'
+        self.HostFancyName=AMON_CONFIG.get('database','host_name')
+        self.UserFancyName=AMON_CONFIG.get('database','username')
+        self.PasswordFancy=AMON_CONFIG.get('database','password')
+        self.DBFancyName=AMON_CONFIG.get('database','realtime_dbname')
         self.DBFancyName2='AMON_test2'
         self.StreamFancyName=0
         self.EventID=1
         self.EventRev=0
-        self.TimeStart='2008-01-01 00:00:00'
+        self.TimeStart='2020-04-01 00:00:00'
         self.TimeSlice=10000 # seconds
-        self.TimeStop='2010-01-01 00:00:00'  # not used for now
-        self.AlertStream=2
+        self.TimeStop='2020-04-02 00:00:00'  # not used for now
+        self.AlertStream=1
         self.AlertRev=0
         self.AlertID=0
         self.ParameterName="energy"
         self.StreamFancyName2=1
         self.EventID2=0
         self.EventRev2=0
-        self.TimeStart2='2000-01-01 00:00:00'
+        self.TimeStart2='2020-03-01 00:00:00'
 
     def tearDown(self):
         # no tear down actions yet
@@ -46,9 +47,9 @@ class TestDBRead(unittest.TestCase):
                                   self.PasswordFancy, self.DBFancyName)
         print(eventPrint.forprint())
 
-    def testReadTimeSlice(self):
+    def testReadTimeSliceStreams(self):
         print('Testing read_event_timeslice module')
-        eventPrintList=db_read.read_event_timeslice(self.TimeStart, self.TimeSlice,
+        eventPrintList=db_read.read_event_timeslice_streams([self.StreamFancyName,self.StreamFancyName2],self.TimeStart, self.TimeSlice,
                                  self.HostFancyName,self.UserFancyName,
                                   self.PasswordFancy, self.DBFancyName)
         for eventPrint in eventPrintList:
